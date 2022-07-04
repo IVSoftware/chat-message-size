6245443eef2bd4d01ab8a6f195f3bc5de52e9fd9
Tom Gregor <tgregor@ivsoftware.com>
2022-07-03 18:02:05 -0600

    Add project files.

---
+++ max_textbox_size/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace max_textbox_size
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCompose = new System.Windows.Forms.TextBox();
+            this.MessageField = new System.Windows.Forms.Panel();
+            this.buttonMe = new System.Windows.Forms.Button();
+            this.buttonYou = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxCompose
+            // 
+            this.textBoxCompose.Location = new System.Drawing.Point(32, 12);
+            this.textBoxCompose.Name = "textBoxCompose";
+            this.textBoxCompose.PlaceholderText = "What\'s on your mind?";
+            this.textBoxCompose.Size = new System.Drawing.Size(519, 31);
+            this.textBoxCompose.TabIndex = 1;
+            this.textBoxCompose.TabStop = false;
+            // 
+            // MessageField
+            // 
+            this.MessageField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MessageField.Location = new System.Drawing.Point(32, 56);
+            this.MessageField.Name = "MessageField";
+            this.MessageField.Size = new System.Drawing.Size(624, 1165);
+            this.MessageField.TabIndex = 6;
+            // 
+            // buttonMe
+            // 
+            this.buttonMe.Enabled = false;
+            this.buttonMe.Font = new System.Drawing.Font("Arial Narrow", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonMe.Location = new System.Drawing.Point(558, 13);
+            this.buttonMe.Name = "buttonMe";
+            this.buttonMe.Size = new System.Drawing.Size(46, 30);
+            this.buttonMe.TabIndex = 7;
+            this.buttonMe.Text = "ME";
+            this.buttonMe.UseVisualStyleBackColor = true;
+            // 
+            // buttonYou
+            // 
+            this.buttonYou.Enabled = false;
+            this.buttonYou.Font = new System.Drawing.Font("Arial Narrow", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonYou.Location = new System.Drawing.Point(610, 13);
+            this.buttonYou.Name = "buttonYou";
+            this.buttonYou.Size = new System.Drawing.Size(46, 30);
+            this.buttonYou.TabIndex = 7;
+            this.buttonYou.Text = "YOU";
+            this.buttonYou.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 1260);
+            this.Controls.Add(this.buttonYou);
+            this.Controls.Add(this.buttonMe);
+            this.Controls.Add(this.MessageField);
+            this.Controls.Add(this.textBoxCompose);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxCompose;
+        private System.Windows.Forms.Panel MessageField;
+        private System.Windows.Forms.Button buttonMe;
+        private System.Windows.Forms.Button buttonYou;
+    }
+}
+
---
+++ max_textbox_size/MainForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace max_textbox_size
+{
+    public partial class MainForm : Form
+    {
+        const int LIMIT = 250;
+        public MainForm()
+        {
+            InitializeComponent();
+            MessageField.ControlAdded += (sender, e) =>
+            {
+                e.Control.MaximumSize = new Size(LIMIT, int.MaxValue);
+                var lastVisible =
+                    MessageField.Controls
+                    .Cast<Control>()
+                    .LastOrDefault(control => control.Visible);                
+                int spacer = 0;
+                if(lastVisible != null)
+                {
+                    switch (e.Control.Name)
+                    {
+                        case "textBox": 
+                            spacer = 2; 
+                            break;
+                        case "timeLabel":
+                            if(lastVisible.Name == "textBox")
+                            {
+                                var minWidth = Math.Max(lastVisible.Width, e.Control.Width);
+                                lastVisible.MinimumSize = e.Control.MinimumSize = new Size(minWidth, 0);
+                                if(lastVisible.Tag.ToString() == "right")
+                                {
+                                    lastVisible.Location = new Point(
+                                        MessageField.Width - lastVisible.Width,
+                                        lastVisible.Location.Y);
+                                }
+                            }
+                            spacer = 0;  break;
+                        default: throw new NotImplementedException();    
+                    }
+                    switch (e.Control.Tag.ToString())
+                    {
+                        case "left": e.Control.Location =
+                            e.Control.Location = new Point(
+                                0,
+                                lastVisible.Location.Y + lastVisible.Height + spacer);
+                            break;
+                        case "right":
+                            e.Control.Location = new Point(
+                                MessageField.Width - e.Control.Width,
+                                lastVisible.Location.Y + lastVisible.Height + spacer);
+                            break;
+                    }
+                }
+                e.Control.Visible = true;
+            };
+            buttonMe.Click += (sender, e) =>
+                TextBubble(textBoxCompose.Text, "left", $"{DateTime.Now}");
+
+            buttonYou.Click += (sender, e) =>
+                TextBubble(textBoxCompose.Text, "right", $"{DateTime.Now}");
+
+            textBoxCompose.TextChanged += (sender, e) =>
+               buttonMe.Enabled = buttonYou.Enabled = !string.IsNullOrWhiteSpace(textBoxCompose.Text);
+        }
+        void TextBubble(string message, string side, string time)
+        {
+            Label textBox = new Label()
+            {
+                Name = nameof(textBox),
+                Visible = false,
+                BackColor = Color.White,
+                Font = new Font("SergioUI", 14),
+                Padding = new Padding(2, 2, 2, 2),
+                AutoSize = true,
+                Text = message,
+                BorderStyle = BorderStyle.FixedSingle, // To see what we're doing
+                Tag = side,
+            };
+            MessageField.Controls.Add(textBox);
+
+            Label timeLabel = new Label
+            {
+                Name = nameof(timeLabel),
+                Visible = false,
+                BackColor = Color.Transparent,
+                Font = new Font("Microsoft Sans Serif", 5),
+                Padding = new Padding(2, 2, 2, 2),
+                AutoSize = true,
+                Text = time,
+                BorderStyle = BorderStyle.FixedSingle,  // To see what we're doing
+                Width = textBox.Width,
+                Location = new Point(textBox.Location.X, textBox.Location.Y + textBox.Height),
+                Tag = side,
+            };
+            MessageField.Controls.Add(timeLabel);
+            textBoxCompose.Clear();
+        }
+    }
+}
